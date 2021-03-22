0e0e0d0a3c1c4f4e9f2a769e277d9b6804163307
Ibrahim Özkan <ibrahim.oezkan2@gmail.com>
2021-03-22 17:17:32 +0100

    Coimmit

--- index.js
+++ index.js
@@ -2,26 +2,62 @@ var port = process.env.PORT || 3000;
 const io = require('socket.io')(port);
 
 console.log(port);
-
-
+const pass = "test123";
+var data;
 io.on("connection", function (socket) {
     console.log("A user cxonnected");
     console.log(socket.id);
     //socket.emit("addFinger",'{"type":"changeMod","currentMod":"addFinger","fingerId":"2"}');
-    io.emit("serverAccepted","true");
-    socket.on("chipMessage",(msg) =>{
-        
-        console.log("FingerId:" + msg.fingerId);
-        console.log("statusDoor:" + msg.statusDoor);
-        console.log("statusBell:" + msg.statusBell);
-        console.log("statusFinger:" + msg.statusFinger);
-        io.emit("espMessage",msg);
+    socket.on("loginServer", (msg) => {
+
+        if (msg == pass) {
+            io.emit("returnPass", "true");
+        } else {
+            io.emit("returnPass", "false");
+        }
     });
-    socket.on("test",(msg)=> {
-        console.log(msg);
+    io.emit("serverAccepted", "true");
+    socket.on("chipMessage", (msg) => {
+        if (msg != null) {
+            data = msg;
+            console.log(JSON.stringify(msg));
+            if (msg.hasOwnProperty("fingerId")) {
+                console.log("FingerId:" + msg.fingerId);
+            }
+            if (msg.hasOwnProperty("statusDoor")) {
+                if (msg.statusDoor == "opened") {
+                    msg.statusDoor = true;
+
+                } else if (msg.statusDoor == "closed") {
+                    msg.statusDoor = false;
+                }
+                console.log("statusDoor:" + msg.statusDoor);
+            }
+            if (msg.hasOwnProperty("statusBell")) {
+                console.log("statusBell:" + msg.statusBell);
+            }
+            if (msg.hasOwnProperty("statusFinger")) {
+
+                if (msg.statusFinger == "notDetected") {
+                    msg.statusFinger = false;
+                } else if (msg.statusFinger == "fingerSucceed") {
+                    msg.statusFinger = true;
+                }
+                console.log("statusFinger:" + msg.statusFinger);
+            }
+
+            io.emit("espMessage", msg);
+        }
     });
-   
-    socket.on("disconnect",()=>{
+    socket.on("requestData", (msg) => {
+        //io.emit("toUno", "{\"currentMod\":\"requestData\"}");
+        io.emit("espMessage",data);
+    })
+    socket.on("test", (msg) => {
+        // console.log(msg);
+    });
+
+    socket.on("disconnect", () => {
         console.log("A user disconnected");
     })
 })
